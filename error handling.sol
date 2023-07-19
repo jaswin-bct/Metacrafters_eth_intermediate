8823cd17c1dcddd00b3929ab0503de343426e200
Jaswin S <112770921+jaswin-bct@users.noreply.github.com>
2023-07-19 11:39:56 +0530

    update

---
+++ error handling.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Eligibility {
+    function check(uint256 _percent) public pure returns (string memory) {
+
+        require(_percent >= 0 && _percent <= 100, "Invalid percentage");  
+        // validation 
+        
+        if (_percent < 75) {
+            revert("You aren't eligible");  
+            // 
+
+
+        } else {
+            assert(_percent >= 75);
+              // internal considtency
+              
+            
+            return "Congratulations! You are eligible for the scholarship";
+        }
+
+    }
+}
